40e3a6ee631076a4a2f6ce35c85ed9341eb2a718
larrylai19 <stji9gjz2@gmail.com>
2021-09-08 11:11:29 +0800

    Create project HelloAlert

---
+++ Yzu2021SummerCamp/HelloAlert/HelloAlert/HelloAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAlertApp.swift
+//  HelloAlert
+//
+//  Created by Larry - 1024 on 2021/9/8.
+//
+
+import SwiftUI
+
+@main
+struct HelloAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
